a1688e6b57535d3352921bfca34b58f88108a0f4
JD Wong <jdmswong@gmail.com>
2023-08-22 09:35:06 -0700

    comsolidate individual repos, main mounts

--- main_old/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React, { render } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './app';
-import './index.css';
-
-const domNode = document.getElementById('app');
-const root = createRoot(domNode);
-root.render(<App />);
